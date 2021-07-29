14d3dce0ad7bd69cc8f1c2566966da7ffbc01b04
Margarita Morozova <ritamorozova@yahoo.com>
2021-07-29 13:01:39 -0700

    first commit

---
+++ src/actions/actionTypes.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+    CREATE_COURSE: "CREATE_COURSE",
+    UPDATE_COURSE: "UPDATE_COURSE",
+    DELETE_COURSE: "DELETE_COURSE",
+    LOAD_COURSES: "LOAD_COURSES"
+  };
